7aa457c18e0abe7c7cd48b164adbf33b24489f3c
Fabio Nebbia <fabionebbia97@gmail.com>
2020-10-26 02:26:50 +0100

    Add TestEvaluator helper module

---
+++ test/support/test_evaluator.ex
@@ -0,0 +1,51 @@
+defmodule TestEvaluator do
+  alias Fika.ErlTranslate
+
+  def eval(code, bindings \\ [])
+
+  # Evaluates a string representing an expression.
+  #
+  # Usage examples:
+  #   1) test "add has less precedence than mult" do
+  #        {7, _} = eval("1 + a * 3", [{:a, 2}])
+  #      end
+  #
+  #   2) test "match operator" do
+  #        {5, [a: 5]} = eval("a = 5", [])
+  #      end
+  def eval(str_exp, bindings) when is_binary(str_exp) do
+    case TestParser.expression(str_exp) do
+      {:ok, [parsed], _, _, _, _} ->
+        {:value, evaluated, new_bindings} =
+          parsed
+          |> ErlTranslate.translate_expression()
+          |> :erl_eval.expr(bindings)
+
+        {evaluated, new_bindings}
+
+      err ->
+        err
+    end
+  end
+
+  # Evaluates a piece of AST representing an expression.
+  #
+  # Usage example:
+  #   test "erlang ast" do
+  #     exp_ast = {
+  #       :bin,
+  #       1,
+  #       [
+  #         {:bin_element, 1, {:string, 1, 'Hello'}, :default, :default},
+  #         {:bin_element, 1, {:string, 1, ' '}, :default, :default},
+  #         {:bin_element, 1, {:string, 1, 'World'}, :default, :default}
+  #       ]
+  #     }
+  #
+  #     {"Hello World", _} = eval(exp_ast)
+  #   end
+  def eval(ast_exp, bindings) when is_tuple(ast_exp) do
+    {:value, evaluated, new_bindings} = :erl_eval.expr(ast_exp, bindings)
+    {evaluated, new_bindings}
+  end
+end
