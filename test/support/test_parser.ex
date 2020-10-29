9538355b96b2b39d68d1c4e4e4cafa99d666493d
Fabio Nebbia <fabionebbia@users.noreply.github.com>
2020-10-29 21:48:57 +0100

    Use evaluation to test for operators precedence (#41)

--- test/support/test_parser.ex
+++ test/support/test_parser.ex
@@ -14,8 +14,36 @@ defmodule TestParser do
   allow_space = parsec({Common, :allow_space})
   parse_type = parsec({Types, :parse_type})
 
-  def expression!(str) do
-    {:ok, [result], _rest, _context, _line, _byte_offset} = expression(str)
+  @vertical_space ["\n", "\r"]
+  @horizontal_space ["\s", "\t"]
+  @space @vertical_space ++ @horizontal_space ++ ["# Dummy comment\n"]
+
+  # Returns a random combination of horizontal space symbols
+  def h_space(length \\ 5), do: do_space(@horizontal_space, length)
+
+  # Returns a random combination of space symbols (both horizontal and vertical)
+  def space(length \\ 5), do: do_space(@space, length)
+
+  # Returns a random combination of space symbols which is
+  # guaranteed to contain at leat one occurence of any of them
+  defp do_space(symbols, length) do
+    n = :rand.uniform(length)
+    more = for _ <- 1..n, do: Enum.random(symbols)
+
+    symbols
+    |> Kernel.++(more)
+    |> Enum.shuffle()
+    |> Enum.join("")
+  end
+
+  def expression!(str), do: do_parse(&expression/1, str)
+
+  def function_def!(str), do: do_parse(&function_def/1, str)
+
+  def type_str!(str), do: do_parse(&type_str/1, str)
+
+  defp do_parse(parser, str) do
+    {:ok, [result], _rest, _context, _line, _byte_offset} = parser.(str)
     result
   end
 
