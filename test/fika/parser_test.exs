c41f022bb24f837716eb3236ce974a6f7ed1dd36
Fabio Nebbia <fabionebbia@users.noreply.github.com>
2020-10-30 15:37:18 +0100

    Add comparison operators for Int (#42)

--- test/fika/parser_test.exs
+++ test/fika/parser_test.exs
@@ -44,12 +44,35 @@ defmodule Fika.ParserTest do
     # ! -         Unary           Boolean not and arithmetic unary minus
     # * /         Left            Arithmetic binary mult and div
     # + -         Left            Arithmetic binary plus and minus
-    # < > <= >=   Left            Comparison                                Implement
-    # == !=       Left            Comparison                                Implement
+    # < > <= >=   Left            Comparison
+    # == !=       Left            Comparison
     # &           Left            Boolean AND
     # |           Left            Boolean OR
     # =           Right           Match operator                            Parser refactoring
     # &           Unary           Capture operator                          Parser refactoring
+
+    # These operators cannot be combined to form a valid expression but we test the parser anyway
+    test "arithmetic binary * and / have less precedence than unary !" do
+      assert {
+               :call,
+               {:*, {1, 0, 14}},
+               [
+                 {:integer, {1, 0, 1}, 1},
+                 {:call, {:!, {1, 0, 14}}, [{:identifier, {1, 0, 14}, :something}], :kernel}
+               ],
+               :kernel
+             } == TestParser.expression!("1 * !something")
+
+      assert {
+               :call,
+               {:/, {1, 0, 14}},
+               [
+                 {:call, {:!, {1, 0, 10}}, [{:identifier, {1, 0, 10}, :something}], :kernel},
+                 {:integer, {1, 0, 14}, 2}
+               ],
+               :kernel
+             } == TestParser.expression!("!something / 2")
+    end
 
     test "arithmetic binary * and / have less precedence than unary -" do
       assert {:ok, {-2, "Int", _}} = TE.eval("1 * -2")
@@ -61,8 +84,51 @@ defmodule Fika.ParserTest do
       assert {:ok, {3.0, "Float", _}} = TE.eval("1 * 5 - 4 / 2")
     end
 
-    test "boolean & has less precedence than !" do
-      assert {:ok, {false, "Bool", _}} = TE.eval("!true & :false")
+    test "comparison operators <, >, <= and >= have less precedence than arithmetic + and -" do
+      assert {:ok, {false, "Bool", _}} = TE.eval("1 + 2 < 4 - 1")
+      assert {:ok, {true, "Bool", _}} = TE.eval("1 + 2 <= 4 - 1")
+      assert {:ok, {false, "Bool", _}} = TE.eval("5 - 2 > 2 + 1")
+      assert {:ok, {true, "Bool", _}} = TE.eval("5 - 2 >= 2 + 1")
+    end
+
+    # TODO: Test using evaluation instead of AST as soon as == and != support booleans.
+    #   Replace the content of this test with something like:
+    #     assert {:ok, {true, "Bool", _}} = TE.eval("1 < 1 == 2 > 2")
+    #     assert {:ok, {true, "Bool", _}} = TE.eval("1 <= 1 != 2 >= 3")
+    test "comparison operators == and != have less precedence than <, >, <=, >=" do
+      # Just for now, we test the parser for precedence
+      assert {
+               :call,
+               {:==, {1, 0, 14}},
+               [
+                 {:call, {:<, {1, 0, 5}}, [{:integer, {1, 0, 1}, 1}, {:integer, {1, 0, 5}, 1}],
+                  :kernel},
+                 {:call, {:>, {1, 0, 14}}, [{:integer, {1, 0, 10}, 2}, {:integer, {1, 0, 14}, 2}],
+                  :kernel}
+               ],
+               :kernel
+             } == TestParser.expression!("1 < 1 == 2 > 2")
+
+      assert {
+               :call,
+               {:!=, {1, 0, 16}},
+               [
+                 {:call, {:<=, {1, 0, 6}}, [{:integer, {1, 0, 1}, 1}, {:integer, {1, 0, 6}, 1}],
+                  :kernel},
+                 {:call, {:>=, {1, 0, 16}},
+                  [{:integer, {1, 0, 11}, 2}, {:integer, {1, 0, 16}, 3}], :kernel}
+               ],
+               :kernel
+             } == TestParser.expression!("1 <= 1 != 2 >= 3")
+
+      # Then we test == and != actually work
+      assert {:ok, {true, "Bool", _}} = TE.eval("1 == 1")
+      assert {:ok, {false, "Bool", _}} = TE.eval("1 != 1")
+    end
+
+    test "boolean & has less precedence than == and !=" do
+      assert {:ok, {true, "Bool", _}} = TE.eval("1 == 1 & 1 != 2")
+      assert {:ok, {false, "Bool", _}} = TE.eval("2 == 1 & 1 != 2")
     end
 
     test "boolean | has less precedence than &" do
@@ -73,6 +139,7 @@ defmodule Fika.ParserTest do
     test "expressions in parenthesis have higher precedence than anything else" do
       assert {:ok, {1, "Int", _}} = TE.eval("-(-1)")
       assert {:ok, {6, "Int", _}} = TE.eval("(3 - 1) * (1 + 2)")
+      assert {:ok, {true, "Bool", _}} = TE.eval("2 >= (1 + 1)")
       assert {:ok, {true, "Bool", _}} = TE.eval("true & (false | true)")
     end
   end
@@ -91,9 +158,13 @@ defmodule Fika.ParserTest do
       assert {:ok, {2.0, "Float", _}} = TE.eval("4#{TP.h_space()}/#{TP.space()}2")
       assert {:ok, {7, "Int", _}} = TE.eval("3#{TP.h_space()}+#{TP.space()}4")
       assert {:ok, {1, "Int", _}} = TE.eval("3#{TP.h_space()}-#{TP.space()}2")
-
+      assert {:ok, {true, "Bool", _}} = TE.eval("2#{TP.h_space()}<#{TP.space()}3")
+      assert {:ok, {true, "Bool", _}} = TE.eval("2#{TP.h_space()}<=#{TP.space()}3")
+      assert {:ok, {true, "Bool", _}} = TE.eval("2#{TP.h_space()}>#{TP.space()}1")
+      assert {:ok, {true, "Bool", _}} = TE.eval("2#{TP.h_space()}>=#{TP.space()}1")
+      assert {:ok, {true, "Bool", _}} = TE.eval("2#{TP.h_space()}==#{TP.space()}2")
+      assert {:ok, {true, "Bool", _}} = TE.eval("2#{TP.h_space()}!=#{TP.space()}1")
       assert {:ok, {true, "Bool", _}} = TE.eval("true#{TP.h_space()}&#{TP.space()}true")
-
       assert {:ok, {true, "Bool", _}} = TE.eval("false#{TP.h_space()}|#{TP.space()}true")
     end
 
@@ -105,9 +176,13 @@ defmodule Fika.ParserTest do
       assert {:error, ^msg, _, _, _, _} = TE.eval("3#{TP.space()}/#{TP.space()}2")
       assert {:error, ^msg, _, _, _, _} = TE.eval("3#{TP.space()}+#{TP.space()}4")
       assert {:error, ^msg, _, _, _, _} = TE.eval("3#{TP.space()}-#{TP.space()}2")
-
+      assert {:error, ^msg, _, _, _, _} = TE.eval("2#{TP.space()}<#{TP.space()}3")
+      assert {:error, ^msg, _, _, _, _} = TE.eval("2#{TP.space()}<=#{TP.space()}3")
+      assert {:error, ^msg, _, _, _, _} = TE.eval("2#{TP.space()}>#{TP.space()}1")
+      assert {:error, ^msg, _, _, _, _} = TE.eval("2#{TP.space()}>=#{TP.space()}1")
+      assert {:error, ^msg, _, _, _, _} = TE.eval("2#{TP.space()}==#{TP.space()}2")
+      assert {:error, ^msg, _, _, _, _} = TE.eval("2#{TP.space()}!=#{TP.space()}1")
       assert {:error, ^msg, _, _, _, _} = TE.eval("true#{TP.space()}&#{TP.space()}true")
-
       assert {:error, ^msg, _, _, _, _} = TE.eval("false#{TP.space()}|#{TP.space()}true")
     end
 
