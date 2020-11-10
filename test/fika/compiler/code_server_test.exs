4369945f9f4dac586c786ce4addff19ff096d084
Emil Soman <emil.soman@gmail.com>
2020-11-10 05:35:57 +0530

    Fix tests

--- test/fika/compiler/code_server_test.exs
+++ test/fika/compiler/code_server_test.exs
@@ -1,5 +1,5 @@
 defmodule Fika.Compiler.CodeServerTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   alias Fika.Compiler.CodeServer
 
--- test/fika/compiler/module_compiler_test.exs
+++ test/fika/compiler/module_compiler_test.exs
@@ -1,5 +1,5 @@
 defmodule Fika.Compiler.ModuleCompilerTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   alias Fika.Compiler.{
     ModuleCompiler
