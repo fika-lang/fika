f030e2a509b0123ec19cac33d84606a7d6f14780
Emil Soman <emil.soman@gmail.com>
2020-11-10 18:14:14 +0530

    Add failure cases to ModuleCompiler test

--- test/fika/compiler/module_compiler_test.exs
+++ test/fika/compiler/module_compiler_test.exs
@@ -6,7 +6,7 @@ defmodule Fika.Compiler.ModuleCompilerTest do
   }
 
   test "given a file with fika code, returns the compiled binary" do
-    module = Path.join(System.tmp_dir!(), "temp") |> String.to_atom()
+    module = Path.join(System.tmp_dir!(), "foo") |> String.to_atom()
     temp_file = "#{module}.fi"
 
     str = """
@@ -17,7 +17,47 @@ defmodule Fika.Compiler.ModuleCompilerTest do
 
     File.write!(temp_file, str)
 
-    {:ok, ^module, ^temp_file, _binary} = ModuleCompiler.compile(module)
+    assert {:ok, ^module, ^temp_file, _binary} = ModuleCompiler.compile(module)
+
+    File.rm!(temp_file)
+  end
+
+  test "returns error when file doesn't exist" do
+    module = Path.join(System.tmp_dir!(), "foo") |> String.to_atom()
+
+    assert {:error, "Cannot read file #{module}.fi: :enoent"} == ModuleCompiler.compile(module)
+  end
+
+  test "returns error when file cannot be parsed" do
+    module = Path.join(System.tmp_dir!(), "foo") |> String.to_atom()
+
+    temp_file = "#{module}.fi"
+
+    str = """
+    function foo = "hello world"
+    """
+
+    File.write!(temp_file, str)
+
+    assert {:error, "Parse error"} == ModuleCompiler.compile(module)
+
+    File.rm!(temp_file)
+  end
+
+  test "returns error when type check fails" do
+    module = Path.join(System.tmp_dir!(), "foo") |> String.to_atom()
+
+    temp_file = "#{module}.fi"
+
+    str = """
+    fn foo : Int do
+      "Hello world"
+    end
+    """
+
+    File.write!(temp_file, str)
+
+    assert {:error, "Type check error"} == ModuleCompiler.compile(module)
 
     File.rm!(temp_file)
   end
