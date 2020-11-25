4c402cd6542f512e44f99d98bc60a5c86607bdc3
Emil Soman <emil.soman@gmail.com>
2020-11-25 21:36:15 +0530

    Comment out bakeware release step until we tackle #54

--- lib/mix/tasks/compile.fika.ex
+++ lib/mix/tasks/compile.fika.ex
@@ -2,11 +2,23 @@ defmodule Mix.Tasks.Compile.Fika do
   use Mix.Task.Compiler
 
   alias Fika.Code
+  require Logger
 
   def run(_args) do
+    level = Logger.level()
+    Logger.configure(level: :warn)
     {:ok, _} = Application.ensure_all_started(:fika)
 
-    Code.compile_to_path("fika/kernel", Mix.Project.compile_path())
+    dest = Mix.Project.compile_path()
+    default_modules = ["fika/io"]
+
+    Enum.each(default_modules, fn module ->
+      Logger.info("Compiling #{module}")
+      Code.compile_to_path(module, dest)
+    end)
+
+    Application.stop(:fika)
+    Logger.configure(level: level)
 
     :ok
   end
